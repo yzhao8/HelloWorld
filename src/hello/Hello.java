246545b2ee39b40cff4d911c60553a56ee6964b5
yzhao8 <37639891+yzhao8@users.noreply.github.com>
2018-03-25 17:26:00 -0400

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println(" test egit starting from eclipse");
 		System.out.println(" feteched from eclipse");
+		System.out.println(" test pull from remote git hub");
 	}
 
 }

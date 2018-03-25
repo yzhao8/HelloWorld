e26edde01b02624dd1121d47820c6dbb0e447ff7
yzhao8 <yzhao8@hotmail.com>
2018-03-25 17:18:34 -0400

    fetched from remote

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println(" test egit starting from eclipse");
-		//
+		System.out.println(" feteched from eclipse");
 	}
 
 }

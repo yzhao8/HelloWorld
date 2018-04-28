acb0ee76a3d5e7acdaf519777781177cad4ecccb
yzhao8 <yzhao8@hotmail.com>
2018-04-28 10:58:31 -0400

    add one line and test IntelliJ commit

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		System.out.println(" test egit starting from eclipse");
 		System.out.println(" feteched from eclipse");
 		System.out.println(" test pull from remote git hub");
+		System.out.println(" Just test add and commit and push from intelliJ");
 	}
 
 }

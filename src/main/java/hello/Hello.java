f2f8b9e81fa68714702ef28bc9efebee0752ecea
yzhao8 <yzhao8@hotmail.com>
2018-05-12 19:53:20 -0400

    Revert "update fix1"
    This reverts commit c5b7bc3dee47589af51e248c4b22d3f66f95adc5.
    
    # Conflicts:
    #	src/hello/Fix1.java

---
+++ src/main/java/hello/Hello.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println(" test egit starting from eclipse");
+		System.out.println(" feteched from eclipse");
+		System.out.println(" test pull from remote git hub");
+		System.out.println(" Just test add and commit and push from intelliJ");
+		System.out.println(" swtich to develop1");
+	}
+
+}

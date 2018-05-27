1d45bdc78cadce126cdc5e912a25056fd2a1c549
yzhao8 <yzhao8@hotmail.com>
2018-05-27 10:52:36 -0400

    add new method state() and test commit reword

--- src/main/java/hello/NewHello.java
+++ src/main/java/hello/NewHello.java
@@ -1,6 +1,13 @@
 package hello;
 
 public class NewHello {
+
+    public String state(String s) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(s);
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         System.out.println(" test add to develop1");
     }

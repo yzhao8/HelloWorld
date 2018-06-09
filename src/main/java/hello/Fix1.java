f7dfacf39d26f0f071b3182129daabfe0890400c
yzhao8 <yzhao8@hotmail.com>
2018-06-09 10:20:45 -0400

    add one more function and run on a new branch

--- src/main/java/hello/Fix1.java
+++ src/main/java/hello/Fix1.java
@@ -1,8 +1,13 @@
 package hello;
 
 public class Fix1 {
+    private String f = "Test";
     // test merge
     protected static String comeFix1() {
         return "come from fix1 but merge";
+    }
+
+    public void setFix(String f) {
+        this.f = f;
     }
 }

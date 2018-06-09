87789d227e2b82822f4010ea2814026eec424731
yzhao8 <yzhao8@hotmail.com>
2018-06-09 10:17:25 -0400

    update return of Fix1 fucntion to check pull/merge

--- src/main/java/hello/Fix1.java
+++ src/main/java/hello/Fix1.java
@@ -1,7 +1,8 @@
 package hello;
 
 public class Fix1 {
+    // test merge
     protected static String comeFix1() {
-        return "come from fix1";
+        return "come from fix1 but merge";
     }
 }

cba2b9ca3efb440c743f2b2b90d47a8984579222
yzhao8 <yzhao8@hotmail.com>
2018-06-09 10:47:01 -0400

    new function

--- src/main/java/hello/Fix2.java
+++ src/main/java/hello/Fix2.java
@@ -8,4 +8,10 @@ public class Fix2 {
     public static void main(String[] args) {
         comeFix2();
     }
+
+    // test merge and pull
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
